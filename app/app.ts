e0b2ae3202f1f75ae8f2fa3583961be2f64c3d4e
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-05-08 23:01:13 +0700

    add console.log

--- app/app.ts
+++ app/app.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(cors())
 
 app.use(express.static(path.resolve(__dirname, "..", "frontend", "dist")));
+console.log("Static file path:", path.resolve(__dirname, "..", "frontend", "dist"),path.join(__dirname,"..","frontend", "dist", "index.html"));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname,"..","frontend", "dist", "index.html"));
