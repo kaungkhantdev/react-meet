7f69c6f345ed120f37ddbb1d222b750e13a7192d
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-05-08 11:23:30 +0700

    fix

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -3,5 +3,5 @@ import config from './config/config';
 import { server } from './socket';
 
 server.listen(config.port, () => {
-    console.log(`Server running on local:'+ config.port}`)
+    console.log(`Server running on local:'+ ${config.port}`)
 })
