e2a3ca4bdf8362fc8cb046b4aa4b2b797691e581
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-05-08 23:28:32 +0700

    fix

---
+++ dist/app/config/config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    port: process.env.PORT || 8000,
+    app_url: process.env.APP_RUL,
+};
+exports.default = config;
+//# sourceMappingURL=config.js.map
